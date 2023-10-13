f4be9908d34cb38ad2e47605d07a5a1648fcbb48
caiquanyou <951463554@qq.com>
2023-10-13 16:28:29 +0800

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.nn.functional import softmax, cosine_similarity
-
+# import osqp
 # class QP_solver:
 #     def __init__(
 #         self,
@@ -73,7 +73,7 @@ class cell2clusters:
         lambda_g1=1.0,
         lambda_g2=1.0,
         device="cpu",
-        random_state=None,
+        random_state=159,
     ):
         """
             scdata (ndarray): single cell expression matrix. 
@@ -88,6 +88,7 @@ class cell2clusters:
         self.lambda_g1 = lambda_g1
         self.lambda_g2 = lambda_g2
         self.random_state = random_state
+        np.random.seed(seed=self.random_state)
         self.cluster_matrix = torch.tensor(
             np.random.normal(0, 1, (scdata.shape[0], clusters.shape[0])), device=device, requires_grad=True, dtype=torch.float32
         )
@@ -116,8 +117,7 @@ class cell2clusters:
             cluster_probs (ndarray): is the optimized mapping matrix, shape = (number_cells, number_clusters).
         """
 
-        if self.random_state:
-            torch.manual_seed(seed=self.random_state)
+        torch.manual_seed(seed=self.random_state)
         optimizer = torch.optim.Adam([self.cluster_matrix], lr=learning_rate)
         for t in range(num_epochs):
             if print_each is None or t % print_each != 0:
@@ -146,16 +146,13 @@ class cell2spots:
         ST,
         Gx,
         Gy,
-        Gz=None,
         lambda_gx1=1,
         lambda_gx2=0,
         lambda_gy1=1,
         lambda_gy2=0,
-        lambda_gz1=1,
-        lambda_gz2=0,
         cluster_matrix=None,
         device="cpu",
-        random_state=None,
+        random_state=159,
     ):
         
         self.S = torch.tensor(S, device=device, dtype=torch.float32)
@@ -166,9 +163,8 @@ class cell2spots:
         self.lambda_gx2 = lambda_gx2
         self.lambda_gy1 = lambda_gy1
         self.lambda_gy2 = lambda_gy2
-        self.lambda_gz1 = lambda_gz1
-        self.lambda_gz2 = lambda_gz2
         self.random_state = random_state
+        np.random.seed(seed=self.random_state)
         if cluster_matrix is None:
             self.cluster_matrix = np.random.normal(0, 1, (S.shape[0], ST.shape[0]))
             self.cluster_matrix = torch.tensor(
@@ -184,7 +180,6 @@ class cell2spots:
         self.device = device
         self.x_length = np.int32(self.ST.obs['x'].max() - self.ST.obs['x'].min() +1)
         self.y_length = np.int32(self.ST.obs['y'].max() - self.ST.obs['y'].min() +1)
-        
         self.x_index = self.ST.obs['x'] - self.ST.obs['x'].min()
         self.x_index = torch.tensor(
             self.x_index, device=device, requires_grad=False, dtype=torch.int32
@@ -193,15 +188,6 @@ class cell2spots:
         self.y_index = torch.tensor(
             self.y_index, device=device, requires_grad=False, dtype=torch.int32
         )
-        if Gz is not None:
-            self.Gz = torch.tensor(Gz, device=device, dtype=torch.float32)
-            self.z_length = np.int32(self.ST.obs['z'].max() - self.ST.obs['z'].min() +1)
-            self.z_index = self.ST.obs['z'] - self.ST.obs['z'].min()
-            self.z_index = torch.tensor(
-                self.z_index, device=device, requires_grad=False, dtype=torch.int32
-            )
-        else:
-            self.Gz = Gz
         
     def _generate_Xstrips(self, cluster_probs):
         """
@@ -216,17 +202,9 @@ class cell2spots:
         Generate Y matrix
         """
         mapping_y = torch.zeros((self.y_length, self.S.shape[0]), device=self.device)
-        mapping_y.index_add_(0, self.y_index, M_probs.T)
+        mapping_y.index_add_(0, self.y_index, cluster_probs.T)
         return mapping_y
     
-    def _generate_Zstrips(self, cluster_probs):
-        """
-        Generate Z matrix
-        """
-        mapping_z = torch.zeros((self.z_length, self.S.shape[0]), device=self.device)
-        mapping_z.index_add_(0, self.z_index, cluster_probs.T)
-        return mapping_z
-
     def _loss_fn(self, verbose=True):
         """
         cell2spot loss function.
@@ -254,24 +232,6 @@ class cell2spots:
         main_x_loss2 = (gx2 / self.lambda_gx2).tolist()
         main_y_loss2 = (gy2 / self.lambda_gy2).tolist()
         total_loss = -cos_x - cos_y  + mask_sum
-        if self.Gz is not None:
-            Mz = self._generate_Zstrips(cluster_probs)
-            Gz_pred = torch.matmul(Mz, self.S)
-            gz1 = self.lambda_gz1 * cosine_similarity(Gz_pred, self.Gz, dim=0).mean()
-            gz2 = self.lambda_gz2 * cosine_similarity(Gz_pred, self.Gz, dim=1).mean()
-            main_z_loss1 = (gz1 / self.lambda_gz1).tolist()
-            main_z_loss2 = (gz2 / self.lambda_gz2).tolist()
-            cos_z = gz1 + gz2
-            total_loss = total_loss -cos_z 
-            if verbose:
-                print("total_loss: {:.3f}".format(total_loss.tolist()))
-            return (
-                total_loss,
-                main_x_loss1,
-                main_y_loss1,
-                main_z_loss1,
-                mask_sum)
-        
         if verbose:
             print("total_loss: {:.3f}".format(total_loss.tolist()))
         return (
@@ -290,8 +250,144 @@ class cell2spots:
         Returns:
             M (ndarray): is the optimized mapping matrix, shape = (number_cells, number_spots).
         """
-        if self.random_state:
-            torch.manual_seed(seed=self.random_state)
+        torch.manual_seed(seed=self.random_state)
+        optimizer = torch.optim.Adam([self.cluster_matrix], lr=learning_rate)
+        for t in range(num_epochs):
+            if print_each is None or t % print_each != 0:
+                run_loss = self._loss_fn(verbose=False)
+            else:
+                run_loss = self._loss_fn(verbose=True)
+            loss = run_loss[0]
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        with torch.no_grad():
+            spot_matrix = softmax(self.cluster_matrix, dim=1).cpu().numpy()
+            return spot_matrix
+
+# model for visium
+class cell2spots3D:
+    """
+    This class implements an algorithm for Visium that maps each cell to its spot.
+    """
+    def __init__(
+        self,
+        S,
+        ST,
+        Gx,
+        Gy,
+        Gz,
+        lambda_gx1=1,
+        lambda_gx2=0,
+        lambda_gy1=1,
+        lambda_gy2=0,
+        lambda_gz1=1,
+        lambda_gz2=0,
+        cluster_matrix=None,
+        device="cpu",
+        random_state=159,
+    ):
+        self.S = torch.tensor(S, device=device, dtype=torch.float32)
+        self.ST = ST
+        self.Gx = torch.tensor(Gx, device=device, dtype=torch.float32)
+        self.Gy = torch.tensor(Gy, device=device, dtype=torch.float32)
+        self.Gz = torch.tensor(Gz, device=device, dtype=torch.float32)
+        self.lambda_gx1 = lambda_gx1
+        self.lambda_gx2 = lambda_gx2
+        self.lambda_gy1 = lambda_gy1
+        self.lambda_gy2 = lambda_gy2
+        self.lambda_gz1 = lambda_gz1
+        self.lambda_gz2 = lambda_gz2
+        self.random_state = random_state
+        np.random.seed(seed=self.random_state)
+        if cluster_matrix is None:
+            self.cluster_matrix = np.random.normal(0, 1, (S.shape[0], ST.shape[0]))
+            self.cluster_matrix = torch.tensor(
+                self.cluster_matrix, device=device, requires_grad=True, dtype=torch.float32
+            )
+        else:
+            self.cluster_matrix = cluster_matrix
+            self.cluster_matrix = torch.tensor(
+                self.cluster_matrix, device=device, requires_grad=True, dtype=torch.float32
+            )
+        self.device = device
+        self.x_length = np.int32(self.ST.obs['x'].max() - self.ST.obs['x'].min() +1)
+        self.y_length = np.int32(self.ST.obs['y'].max() - self.ST.obs['y'].min() +1)
+        self.z_length = np.int32(self.ST.obs['z'].max() - self.ST.obs['z'].min() +1)
+        self.x_index = self.ST.obs['x'] - self.ST.obs['x'].min()
+        self.x_index = torch.tensor(
+            self.x_index, device=device, requires_grad=False, dtype=torch.int32
+        )
+        self.y_index = self.ST.obs['y'] - self.ST.obs['y'].min()
+        self.y_index = torch.tensor(
+            self.y_index, device=device, requires_grad=False, dtype=torch.int32
+        )
+        self.z_index = self.ST.obs['z'] - self.ST.obs['z'].min()
+        self.z_index = torch.tensor(
+            self.z_index, device=device, requires_grad=False, dtype=torch.int32
+        )
+        
+    def _generate_Xstrips(self, cluster_probs):
+        """
+        Generate X matrix
+        """
+        mapping_x = torch.zeros((self.x_length, self.S.shape[0]), device=self.device)
+        mapping_x.index_add_(0, self.x_index, cluster_probs.T)
+        return mapping_x
+
+    def _generate_Ystrips(self, cluster_probs):
+        """
+        Generate Y matrix
+        """
+        mapping_y = torch.zeros((self.y_length, self.S.shape[0]), device=self.device)
+        mapping_y.index_add_(0, self.y_index, cluster_probs.T)
+        return mapping_y
+    
+    def _generate_Zstrips(self, cluster_probs):
+        """
+        Generate Z matrix
+        """
+        mapping_z = torch.zeros((self.z_length, self.S.shape[0]), device=self.device)
+        mapping_z.index_add_(0, self.z_index, cluster_probs.T)
+        return mapping_z
+
+    def _loss_fn(self, verbose=True):
+        cluster_probs = softmax(self.cluster_matrix, dim=1)
+        Mx = self._generate_Xstrips(cluster_probs)
+        My = self._generate_Ystrips(cluster_probs)
+        Mz = self._generate_Zstrips(cluster_probs)
+        Gx_pred = torch.matmul(Mx, self.S)
+        Gy_pred = torch.matmul(My, self.S)
+        Gz_pred = torch.matmul(Mz, self.S)
+        x_gv_term = self.lambda_gx1 * cosine_similarity(Gx_pred, self.Gx, dim=0).mean()
+        x_vg_term = self.lambda_gx2 * cosine_similarity(Gx_pred, self.Gx, dim=1).mean()
+        y_gv_term = self.lambda_gy1 * cosine_similarity(Gy_pred, self.Gy, dim=0).mean()
+        y_vg_term = self.lambda_gy2 * cosine_similarity(Gy_pred, self.Gy, dim=1).mean()
+        z_gv_term = self.lambda_gz1 * cosine_similarity(Gz_pred, self.Gz, dim=0).mean()
+        z_vg_term = self.lambda_gz2 * cosine_similarity(Gz_pred, self.Gz, dim=1).mean()        
+        expression_term_x = x_gv_term + x_vg_term
+        expression_term_y = y_gv_term + y_vg_term
+        expression_term_z = z_gv_term + z_vg_term
+        main_x_loss = (x_gv_term / self.lambda_gx1).tolist()
+        main_y_loss = (y_gv_term / self.lambda_gy1).tolist()
+        main_z_loss = (z_gv_term / self.lambda_gz1).tolist()
+        main_x_loss = (x_gv_term / self.lambda_gx1).tolist()
+        main_y_loss = (y_gv_term / self.lambda_gy1).tolist()
+        main_z_loss = (z_gv_term / self.lambda_gz1).tolist()
+        x_vg_reg = (x_vg_term / self.lambda_gx2).tolist()
+        y_vg_reg = (y_vg_term / self.lambda_gy2).tolist()
+        z_vg_reg = (z_vg_term / self.lambda_gz2).tolist()
+        total_loss = -expression_term_x - expression_term_y - expression_term_z
+        if verbose:
+            print("total_loss: {:.3f}".format(total_loss.tolist()))
+        return (
+            total_loss,
+            main_x_loss,
+            main_y_loss,
+            main_z_loss)
+
+    def fit(self, num_epochs, learning_rate=0.1, print_each=100):
+        torch.manual_seed(seed=self.random_state)
         optimizer = torch.optim.Adam([self.cluster_matrix], lr=learning_rate)
         for t in range(num_epochs):
             if print_each is None or t % print_each != 0:
