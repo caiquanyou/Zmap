73298390c38273ecbfd05ff04677ae5d469cb995
caiquanyou <951463554@qq.com>
2023-11-01 15:14:54 +0800

    Update model.py

--- model.py
+++ model.py
@@ -130,6 +130,7 @@ class cell2clusters:
             optimizer.step()
         with torch.no_grad():
             cluster_probs = softmax(self.cluster_matrix, dim=1).cpu().numpy()
+            torch.cuda.empty_cache()
             return cluster_probs
 
 class cell2spots:
@@ -263,6 +264,7 @@ class cell2spots:
             optimizer.step()
         with torch.no_grad():
             spot_matrix = softmax(self.cluster_matrix, dim=1).cpu().numpy()
+            torch.cuda.empty_cache()
             return spot_matrix
 
 # model for visium
@@ -400,4 +402,135 @@ class cell2spots3D:
             optimizer.step()
         with torch.no_grad():
             spot_matrix = softmax(self.cluster_matrix, dim=1).cpu().numpy()
+            torch.cuda.empty_cache()
             return spot_matrix
+
+
+# model for strips(not implemented yet)
+class cell2spots_strips:
+    """
+    This class implements an algorithm that maps each cell to its spot from strips bulk RNA seq.
+    """
+    def __init__(
+        self,
+        S,
+        Gx,
+        Gy,
+        lambda_gx1=1,
+        lambda_gx2=0,
+        lambda_gy1=1,
+        lambda_gy2=0,
+        device="cpu",
+        random_state=159,
+    ):
+        
+        self.S = torch.tensor(S, device=device, dtype=torch.float32)
+        self.Gx = torch.tensor(Gx, device=device, dtype=torch.float32)
+        self.Gy = torch.tensor(Gy, device=device, dtype=torch.float32)
+        self.lambda_gx1 = lambda_gx1
+        self.lambda_gx2 = lambda_gx2
+        self.lambda_gy1 = lambda_gy1
+        self.lambda_gy2 = lambda_gy2
+        self.random_state = random_state
+        np.random.seed(seed=self.random_state)
+        if cluster_matrix is None:
+            self.cluster_matrix = np.random.normal(0, 1, (S.shape[0], ST.shape[0]))
+            self.cluster_matrix = torch.tensor(
+                self.cluster_matrix, device=device, requires_grad=True, dtype=torch.float32
+            )
+        else:
+            self.cluster_matrix = cluster_matrix
+            self.cluster_matrix = torch.tensor(
+                self.cluster_matrix, device=device, requires_grad=True, dtype=torch.float32
+            )
+            # self.rawM = self.M.clone().detach() > 0
+            self.mask = self.cluster_matrix.clone().detach() < 1
+        self.device = device
+        self.x_length = Gx.shape[0]
+        self.y_length = Gy.shape[0]
+        self.x_index = self.ST.obs['x'] - self.ST.obs['x'].min()
+        self.x_index = torch.tensor(
+            self.x_index, device=device, requires_grad=False, dtype=torch.int32
+        )
+        self.y_index = self.ST.obs['y'] - self.ST.obs['y'].min()
+        self.y_index = torch.tensor(
+            self.y_index, device=device, requires_grad=False, dtype=torch.int32
+        )
+        
+    def _generate_Xstrips(self, cluster_probs):
+        """
+        Generate X matrix
+        """
+        mapping_x = torch.zeros((self.x_length, self.S.shape[0]), device=self.device)
+        mapping_x.index_add_(0, self.x_index, cluster_probs.T)
+        return mapping_x
+
+    def _generate_Ystrips(self, cluster_probs):
+        """
+        Generate Y matrix
+        """
+        mapping_y = torch.zeros((self.y_length, self.S.shape[0]), device=self.device)
+        mapping_y.index_add_(0, self.y_index, cluster_probs.T)
+        return mapping_y
+    
+    def _loss_fn(self, verbose=True):
+        """
+        cell2spot loss function.
+        """
+        cluster_probs = softmax(self.cluster_matrix, dim=1)
+        mask_sum = torch.masked_select(cluster_probs,self.mask).mean() 
+        Mx = self._generate_Xstrips(cluster_probs)
+        My = self._generate_Ystrips(cluster_probs)
+        dx_pred = torch.log(
+            Mx.T.sum(axis=0) / Mx.shape[1]
+        )  # KL wants the log in first argument
+        dy_pred = torch.log(
+            My.T.sum(axis=0) / My.shape[1]
+        )
+        Gx_pred = torch.matmul(Mx, self.S)
+        Gy_pred = torch.matmul(My, self.S)
+        gx1 = self.lambda_gx1 * cosine_similarity(Gx_pred, self.Gx, dim=0).mean()
+        gx2 = self.lambda_gx2 * cosine_similarity(Gx_pred, self.Gx, dim=1).mean()
+        gy1 = self.lambda_gy1 * cosine_similarity(Gy_pred, self.Gy, dim=0).mean()
+        gy2 = self.lambda_gy2 * cosine_similarity(Gy_pred, self.Gy, dim=1).mean()
+        cos_x = gx1 + gx2
+        cos_y = gy1 + gy2
+        main_x_loss1 = (gx1 / self.lambda_gx1).tolist()
+        main_y_loss1 = (gy1 / self.lambda_gy1).tolist()
+        main_x_loss2 = (gx2 / self.lambda_gx2).tolist()
+        main_y_loss2 = (gy2 / self.lambda_gy2).tolist()
+        total_loss = -cos_x - cos_y  + mask_sum
+        if verbose:
+            print("total_loss: {:.3f}".format(total_loss.tolist()))
+        return (
+            total_loss,
+            main_x_loss1,
+            main_y_loss1,
+            mask_sum)
+
+    def fit(self, num_epochs, learning_rate=0.1, print_each=100):
+        """
+        Run the optimizer and returns the mapping outcome.
+        Args:
+            num_epochs (int): Number of epochs.
+            learning_rate (float): Optional. Learning rate for the optimizer. Default is 0.1.
+            print_each (int): Optional. Prints the loss each print_each epochs. If None, the loss is never printed. Default is 100.
+        Returns:
+            M (ndarray): is the optimized mapping matrix, shape = (number_cells, number_spots).
+        """
+        torch.manual_seed(seed=self.random_state)
+        optimizer = torch.optim.Adam([self.cluster_matrix], lr=learning_rate)
+        for t in range(num_epochs):
+            if print_each is None or t % print_each != 0:
+                run_loss = self._loss_fn(verbose=False)
+            else:
+                run_loss = self._loss_fn(verbose=True)
+            loss = run_loss[0]
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        with torch.no_grad():
+            spot_matrix = softmax(self.cluster_matrix, dim=1).cpu().numpy()
+            torch.cuda.empty_cache()
+            return spot_matrix
+
